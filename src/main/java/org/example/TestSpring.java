6240563ae17cb115679f89111bec289f9a01c599
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-02-15 11:09:28 +0200

    notes, functionality: made notes about DI, make DI using XML.
    Made notes about Depencendy injection.
    Add configuration in XML file for making DI.

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,9 +9,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music musicBean = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
         musicPlayer.playMusic();
 
         context.close();
