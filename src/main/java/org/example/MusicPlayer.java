bc1fcca259186e8db801da87764c147034660470
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-02-15 11:38:14 +0200

    functionality: adding DI to MusicPlayer using setter and file.
    Adding DI to MusicPlayer using setter, XML and properties file.
    Making DI to player using list of musics via constructor.

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,17 +1,47 @@
 package org.example;
 
+import java.util.List;
+
 /**
  * @author Stanislav Hlova
  */
 public class MusicPlayer {
-    private final Music music;
+    private List<Music> musicList;
+    private String name;
+    private int volume;
 
     // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public MusicPlayer() {
     }
 
     public void playMusic() {
-        System.out.printf("Playing: %s", music.getSong());
+        for(Music music : musicList) {
+            System.out.printf("Playing: %s\n", music.getSong());
+        }
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
