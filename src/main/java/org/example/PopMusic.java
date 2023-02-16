09fc8a7306451fba1382437b3abbee726769faf6
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-02-16 10:16:59 +0200

    notes, functionality: making notes about Bean Lifecycle, check bean's init-, destroy-, factory-methods.
    Making notes about lifecycle of the bean.
    Adding the usage the bean lifecycle's methods.

--- src/main/java/org/example/PopMusic.java
+++ src/main/java/org/example/PopMusic.java
@@ -4,8 +4,17 @@ package org.example;
  * @author Stanislav Hlova
  */
 public class PopMusic implements Music{
-    @Override
+    private PopMusic(){}
+    public void init(){
+        System.out.println("Initialization...");
+    }
+    public void destroy(){
+        System.out.println("Destroying...");
+    }    @Override
     public String getSong() {
         return "Blame";
+    }
+    public static PopMusic getPopMusic(){
+        return new PopMusic();
     }
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -8,20 +8,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(comparison);
-
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(10);
-
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+        PopMusic popMusic1 = context.getBean("musicBean", PopMusic.class);
+        PopMusic popMusic2 = context.getBean("musicBean", PopMusic.class);
+        System.out.println(popMusic1==popMusic2);
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(comparison);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
