e3ed93ab26fce365e6a45bbed19a9d85ed96c23f
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-02-17 09:40:05 +0200

    annotations: adding class, which depends on another class with dependency
    Adding class Computer with dependency on MusicPlayer class, which
    depends on Music subclasses for showing the autowiring.

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,9 +9,9 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("myMusicPlayer",MusicPlayer.class);
+        Computer computer = context.getBean("computer", Computer.class);
 
-        musicPlayer.playMusic();
+        computer.playMusic();
 
         context.close();
     }
