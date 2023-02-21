87c6ff3c67d71ce898b436245da3617470a30519
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-02-21 09:39:11 +0200

    notes, functionality: notes about @RequestParam, changing request with annotation.
    Adding some notes about @RequestParam annotation.
    Changing the way of getting the parameters from get request.

--- src/main/java/org/example/controllers/FirstController.java
+++ src/main/java/org/example/controllers/FirstController.java
@@ -3,6 +3,7 @@ package org.example.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,16 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam("surname") String surname) {
 
         System.out.println(name + " " + surname);
 
         return "first/hello";
     }
+
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage() {
         return "first/goodbye";
     }
 }
