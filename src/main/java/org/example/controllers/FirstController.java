d2c1ec6dbdb90114982e12dc797108e7ffb4b4c6
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-02-21 09:52:16 +0200

    notes, functionality: notes about Model, using model to pass information.
    Making notes about Model in Spring MVC.
    Passing information to View using Model.

--- src/main/java/org/example/controllers/FirstController.java
+++ src/main/java/org/example/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +16,10 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam("surname") String surname) {
+                            @RequestParam("surname") String surname,
+                            Model model) {
 
-        System.out.println(name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname + "!");
 
         return "first/hello";
     }
