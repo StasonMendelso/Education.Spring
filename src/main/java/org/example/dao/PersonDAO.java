d686928e97eb6e7fc79f863002ad71c40f82731e
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-02-22 12:50:00 +0200

    notes, database: notes about JDBC, connecting database.
    Making notes about JDBC, PostgreSQL.
    Implementing the connection to the PostgreSQL database.
    Adding database dump.

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -3,6 +3,12 @@ package org.example.dao;
 import org.example.models.Person;
 import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,41 +18,72 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private final List<Person> people;
 
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom", 20, "tom@email.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob", 30, "bob@email.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike", 32, "mike@email.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy", 19, "katy@email.com"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "password";
+
+    private Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM Person")) {
+                while (resultSet.next()) {
+                    Person person = new Person();
+
+                    person.setId(resultSet.getInt("id"));
+                    person.setName(resultSet.getString("name"));
+                    person.setAge(resultSet.getInt("age"));
+                    person.setEmail(resultSet.getString("email"));
+
+                    people.add(person);
+                }
+            }
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
+
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().
-                filter(person -> person.getId() == id)
-                .findAny()
-                .orElse(null);
+//        return people.stream().
+//                filter(person -> person.getId() == id)
+//                .findAny()
+//                .orElse(null);
+        return null;
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() + "'," + person.getAge() + ",'" + person.getEmail() + "');";
+            statement.executeUpdate(SQL);
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
+
     }
 
     public void update(int id, Person person) {
-        Person personToBeUpdated = show(id);
-
-        personToBeUpdated.setName(person.getName());
-        personToBeUpdated.setAge(person.getAge());
-        personToBeUpdated.setEmail(person.getEmail());
+//        Person personToBeUpdated = show(id);
+//
+//        personToBeUpdated.setName(person.getName());
+//        personToBeUpdated.setAge(person.getAge());
+//        personToBeUpdated.setEmail(person.getEmail());
     }
 
     public void delete(int id) {
-        people.removeIf(person -> person.getId() == id);
+//        people.removeIf(person -> person.getId() == id);
     }
 }
