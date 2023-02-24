5c7f76f22a26c182bbf2ff3ff6dfe9143c56b2df
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-02-24 09:20:07 +0200

    notes, configuration: notes about database configuration with file.
    Making notes about database configuration using the separated file.
    Implementing the configuration using the configuration file for
    database.

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -2,18 +2,10 @@ package org.example.dao;
 
 import org.example.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
