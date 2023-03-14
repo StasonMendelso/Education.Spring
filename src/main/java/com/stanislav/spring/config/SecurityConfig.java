3c148547411ef346db8878c3ce2a8b29e034d2b7
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-03-14 09:47:58 +0200

    notes, authentication: notes about another way of authentication
    Making notes about authentication
    Implementing another authentication

--- src/main/java/com/stanislav/spring/config/SecurityConfig.java
+++ src/main/java/com/stanislav/spring/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.stanislav.spring.config;
 
-import com.stanislav.spring.security.AuthProviderImpl;
+import com.stanislav.spring.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
 
@@ -14,16 +18,21 @@ import java.util.Arrays;
  * @author Stanislav Hlova
  */
 @EnableWebSecurity
-@Configuration
 public class SecurityConfig {
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManagerBuilder authenticationManager(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(personDetailsService);
+        return authenticationManagerBuilder;
     }
     @Bean
-    public AuthenticationManager authenticationManager() {
-        return new ProviderManager(Arrays.asList(authProvider));
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
