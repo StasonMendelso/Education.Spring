15522f0ac6853b76ba4b28b2a8f13ea1e1c6abfd
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-03-15 12:22:23 +0200

    notes, security: notes about annotation PreAuthorize,using it in Service
    Making notes about @PreAuthorize.
    Using @PreAuthorize on the Service-layer instead of SecurityConfig
    request mapping.

--- src/main/java/com/stanislav/spring/config/SecurityConfig.java
+++ src/main/java/com/stanislav/spring/config/SecurityConfig.java
@@ -6,9 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
  * @author Stanislav Hlova
  */
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
     private final PersonDetailsService personDetailsService;
 
@@ -31,7 +31,6 @@ public class SecurityConfig {
         httpSecurity
                 .authorizeHttpRequests((auth) -> {
                     auth
-                            .requestMatchers("/admin").hasRole("ADMIN")
                             .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                             .anyRequest().hasAnyRole("USER","ADMIN");
                 })
--- src/main/java/com/stanislav/spring/controllers/HelloController.java
+++ src/main/java/com/stanislav/spring/controllers/HelloController.java
@@ -1,6 +1,8 @@
 package com.stanislav.spring.controllers;
 
 import com.stanislav.spring.security.PersonDetails;
+import com.stanislav.spring.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class HelloController {
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
@@ -25,6 +34,7 @@ public class HelloController {
     }
     @GetMapping("/admin")
     public String showAdminPage(){
+        adminService.doAdminStuff();
         return "admin";
     }
 }
